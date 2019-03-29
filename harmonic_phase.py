d14a9e1bf885c643073cd9d2848e91795c0bfd93
Gabe <gabriel.p.lynch@gmail.com>
2019-03-29 10:15:23 -0500

    fixed square root error

--- harmonic_phase.py
+++ harmonic_phase.py
@@ -1,6 +1,7 @@
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 from scipy.integrate import odeint
 
 m = 1.
@@ -12,8 +13,8 @@ w = 1.
 
 def hoscillator(gc, t, mass, omega):
     qtemp, ptemp = gc
-    q = qtemp*np.sqrt(mass*omega)
-    p = ptemp/np.sqrt(mass*omega)
+    q = qtemp*mass*omega
+    p = ptemp/(mass*omega)
     derivative = [omega*p, -omega*q]
     return derivative
 
@@ -26,15 +27,43 @@ t = np.linspace(0, 10, 101)
 
 sol = odeint(hoscillator, gc0, t, args=(m, w))
 
-fig, ax= plt.subplots()
+fig, ax= plt.subplots(2,1, gridspec_kw = {'height_ratios':[3, 1]})
 
-ax.plot(sol[:,0], sol[:,1])
+ax[0].plot(sol[:,0], sol[:,1])
 
-ax.grid(True)
-ax.set_xlabel('q')
-ax.set_ylabel('p')
+ax[0].grid(True)
+ax[0].set_xlabel('q')
+ax[0].set_ylabel('p')
 
-ax.axhline(y=0, color='k')
-ax.axvline(x=0, color='k')
+ax[0].axhline(y=0, color='k')
+ax[0].axvline(x=0, color='k')
+
+dot, = ax[0].plot(sol[0,0], sol[0,1], 'o')
+osc, = ax[1].plot(sol[0,0], 0, 'o')
+
+def init():
+    dot.set_data([],[])
+    osc.set_data([],[])
+    return dot, osc
+
+def animate(i):
+    q = sol[i,0]
+    p = sol[i, 1]
+    dot.set_data(sol[i, 0], sol[i,1])
+    osc.set_data(sol[i,0], 0)
+    return dot, osc 
+
+ax[1].set_xlim([-1.1, 1.1])
+ax[1].set_ylim([-0.1, 0.1])
+ax[1].get_xaxis().set_ticks([])
+ax[1].get_yaxis().set_ticks([])
+
+
+fps=15.0
+mult=(1.0/fps)*1000
+frames = 101
+
+anim = FuncAnimation(fig, animate, init_func=init, frames=frames, interval=mult, blit=False)
+anim.save('test.gif', dpi=80, writer='imagemagick')
 
 plt.show()
